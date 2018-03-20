ff60f9f1fbfca28a44664f765ac284ca69e3d84a
iliasidash <iliasidash@gmail.com>
2018-03-20 23:44:58 +0500

    Feat: react-live-time was added

--- src/Main/Scoreboard.js
+++ src/Main/Scoreboard.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import styled from 'styled-components';
+import Clock from 'react-live-clock';
+
 import Counter from './Counter';
 
 const Title = styled.h1`
@@ -21,26 +23,64 @@ const TableHeader = styled.th`
   width: calc(100% / 7);
 `;
 
-const Scoreboard = ({ data }) => (
+const Button = styled.button`
+  display: block;
+  margin-left: auto;
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const headers = [
+  {
+    id: 1,
+    text: 'Номер рейса',
+  },
+  {
+    id: 2,
+    text: 'Город',
+  },
+  {
+    id: 3,
+    text: 'Тип самолета',
+  },
+  {
+    id: 4,
+    text: 'Фактическое время',
+  },
+  {
+    id: 5,
+    text: 'Время',
+  },
+  {
+    id: 6,
+    text: 'Статус',
+  },
+];
+
+const Scoreboard = ({ flights, onHandleClick }) => (
   <Grid>
     <Row middle="xs">
       <Col lg={10}>
         <Title> Информационное табло </Title>
       </Col>
       <Col lg={2}>
-        <Counter count={data.flights.length} />
+        <Counter count={flights.length} />
       </Col>
     </Row>
+    <Button onClick={onHandleClick}>Сортировать</Button>
     <Table>
       <TableRow>
-        {data.headers.map(header => <TableHeader key={header.id}>{header.text}</TableHeader>)}
+        {headers.map(header => <TableHeader key={header.id}>{header.text}</TableHeader>)}
       </TableRow>
-      {data.flights.map(flight => (
+      {flights.map(flight => (
         <TableRow key={flight.id}>
           <TableCol>{flight.number}</TableCol>
           <TableCol>{flight.airoport}</TableCol>
           <TableCol>{flight.aircraft}</TableCol>
-          <TableCol>{flight.time}</TableCol>
+          <TableCol>
+            <Clock format="HH:mm:ss" ticking timezone="Asia/Yekaterinburg" />
+          </TableCol>
           <TableCol>{flight.departTime}</TableCol>
           <TableCol>{flight.status}</TableCol>
         </TableRow>
